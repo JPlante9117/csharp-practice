303c79af17329e6c35d2517540af47b17683c9b8
Jacques Plante <jacrplante@gmail.com>
2020-06-01 10:58:31 -0400

    initial commit

---
+++ Giraffe/Book.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Giraffe
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        private int pages;
+        public static int bookCount = 0;
+
+        public Book(string bookTitle, string authorName, int numOfPages)
+        {
+            Console.WriteLine("Creating Book...");
+            title = bookTitle;
+            author = authorName;
+            Pages = numOfPages;
+            bookCount++;
+        }
+
+        public int Pages
+        {
+            get { return pages; }
+            set 
+            {
+                if(value > 0)
+                {
+                    pages = value;
+                } else
+                {
+                    pages = 1;
+                }
+            }
+        }
+
+        public bool isNovel()
+        {
+            if(pages > 100)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Giraffe/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string> { "<name>", "Ana", "Jacques" };
+            foreach (string name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}");
+            }
+        }
+    }
+}
