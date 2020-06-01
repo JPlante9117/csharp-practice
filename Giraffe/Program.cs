9cf1ee9ded255a86bcfe64a82d6e9bb4ed613672
Jacques Plante <jacrplante@gmail.com>
2020-06-01 11:08:37 -0400

    changed file to use list of names for list practice

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -8,10 +8,33 @@ namespace Giraffe
         static void Main(string[] args)
         {
             List<string> names = new List<string> { "<name>", "Ana", "Jacques" };
-            foreach (string name in names)
+
+            names.Add("Kendra");
+            names.Add("Scott");
+            names.Add("Alex");
+            names.Remove("<name>");
+            foreach (var name in names)
             {
-                Console.WriteLine($"Hello {name.ToUpper()}");
+                Console.WriteLine($"Hello, {name}");
+            }
+
+            names.Sort();
+
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hi there, {name}");
+            }
+
+            var index = names.IndexOf("Betty");
+            if (index == -1)
+            {
+                Console.WriteLine("Name not found");
+            }
+            else
+            {
+                Console.WriteLine($"Found {names[index]} is at index {index}");
             }
         }
+
     }
 }
